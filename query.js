bd0e18bf2da4f312274c3e1bd93dd4ec7b5829f7
gumbyferret <yb@converged-systems.com>
2016-01-08 14:42:02 +0000

    Update query.js

--- query.js
+++ query.js
@@ -1,9 +1,12 @@
 //Find All
+var qs = require('querystring');
 var Msg = require('./message');
 //get all the users
 Msg.find({}, function(err, messages) {
   if (err) throw err;
-
+  var data = qs.parse(messages);
+  var jsonString = JSON.stringify(data);
+  var jsonDataObject = JSON.parse(jsonString);
   // object of all the users
-  console.log(messages.message);
+  console.log(jsonString.message);
 });
