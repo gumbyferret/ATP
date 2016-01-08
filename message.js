53f0aabd752945aa6275e2579599dbe1a5e91009
gumbyferret <yb@converged-systems.com>
2016-01-08 10:55:46 +0000

    Create message.js

---
+++ message.js
@@ -0,0 +1,41 @@
+// Use mongoose in the project
+
+var mongoose = require('mongoose');
+
+// Connect to a MongoDB database locally
+
+mongoose.connect('mongodb://localhost/incoming');
+
+// First define the schema
+
+var Schema = mongoose.Schema;
+
+// Create a schema called userSchema.  ALlowed schemaTypes are String, Number, Data, Buffer
+// Boolean, Mixed, ObjectId, Array
+
+var msgSchema = new Schema({
+  name: String,
+  message: String,
+  created_at: Date,
+  delete_flag: Boolean
+});
+
+// Run a function before saving
+// on every save, add the date
+msgSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+  
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+
+var User = mongoose.model('Message', msgSchema);
+
+// make this available to our users in our Node applications
+
+module.exports = Messages;
