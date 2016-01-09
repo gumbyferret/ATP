7cb674ef902485f9d461853a96d5995cc3cce6cf
gumbyferret <yb@converged-systems.com>
2016-01-09 19:10:45 +0000

    Update query2.js

--- query2.js
+++ query2.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Msg = require('./message');
 var app = express();
 
-app.get('/message.html', function (req, res) {
+app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "message.html" );
 })
 
@@ -10,8 +10,8 @@ app.get('/process_get', function (req, res) {
 
    // Prepare output in JSON format
    response = {
-       first_name:req.query.name,
-       last_name:req.query.message
+       name:req.query.name,
+       message:req.query.message
    };
    console.log(response);
    res.end(JSON.stringify(response));
